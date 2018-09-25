7af3e69cde2855925fb7063874e072dd4a1bfefd
Tushar <Tushar@192.168.2.5>
2018-09-25 23:30:31 +0530

    first commit

---
+++ GitDemo1/src/com/Hsbcnet/Testcases/Base.java
@@ -0,0 +1,15 @@
+package com.Hsbcnet.Testcases;
+
+public class Base {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("project started");
+		
+		Login l = new Login();
+		
+		l.sde3Login();
+	}
+
+}
---
+++ GitDemo1/src/com/Hsbcnet/Testcases/Login.java
@@ -0,0 +1,14 @@
+package com.Hsbcnet.Testcases;
+
+public class Login {
+
+		
+	void sde3Login(){
+		
+		System.out.println("This is sde3 login");
+		
+	}
+
+	
+
+}
