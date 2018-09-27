ec2f28aae503d2e3ebf23cabeb65506bfbc5d736
Viraj <Viraj@Viraj-PC>
2018-09-27 22:35:23 +0530

    added spid pp method

--- GitDemo1/src/com/Hsbcnet/Testcases/PriorityPayment.java
+++ GitDemo1/src/com/Hsbcnet/Testcases/PriorityPayment.java
@@ -5,4 +5,9 @@ public class PriorityPayment {
 	void MXCorePP(){
 		System.out.println("This is MX PP core payment");
 	}
+	
+	void MXSPIDPP(){
+		System.out.println("This is MX PP spid payment");
+		
+	}
 }
