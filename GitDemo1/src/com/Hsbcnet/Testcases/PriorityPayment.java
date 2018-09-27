1548a4d799891b672c8ce87e3c03858d62ea05a5
Viraj <Viraj@Viraj-PC>
2018-09-27 22:49:49 +0530

    Added Poland PP

--- GitDemo1/src/com/Hsbcnet/Testcases/PriorityPayment.java
+++ GitDemo1/src/com/Hsbcnet/Testcases/PriorityPayment.java
@@ -2,12 +2,18 @@ package com.Hsbcnet.Testcases;
 
 public class PriorityPayment {
 
-	void MXCorePP(){
+	void MXCorePP() {
 		System.out.println("This is MX PP core payment");
 	}
-	
+
 	void MXSPIDPP(){
 		System.out.println("This is MX PP spid payment");
-		
+		for(int i=2;i<6;i++){
+			System.out.println(i);
+		}
 	}
-}
+	void polandPP(){
+	System.out.println("This is poland PP");
+	}
+	
+	}
